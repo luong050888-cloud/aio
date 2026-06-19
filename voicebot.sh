79c236bdac0cfc523e9b2009380407af3a161a31
luong050888-cloud <luong050888@gmail.com>
2026-06-19 21:06:05 +0700

    Update voicebot.sh

--- voicebot.sh
+++ voicebot.sh
@@ -5,7 +5,7 @@ ADB_DEVICE_PORT="5555"
 ADB_DEVICE="$ADB_DEVICE_IP:$ADB_DEVICE_PORT"
 ADB="adb"
 
-BASE_URL="https://github.com/thutrang0410/aio/releases/download/acb"
+BASE_URL="https://github.com/luong050888-cloud/aio/releases/download/abc"
 PACKAGE_NAME="info.dourok.voicebot"
 
 FREE_APK="free.apk"
@@ -14,7 +14,7 @@ AIBOXPLUS_APK="aibox+.apk"
 DLNA_APK="auto-dlna.apk"
 UNI_SOUND_APK="uni-sound.apk"
 
-log_info() { echo "[CHANGG] $*"; }
+log_info() { echo "[LUONG] $*"; }
 
 open_browser() {
     URL="http://192.168.43.1:8081"
@@ -139,7 +139,7 @@ install_apk() {
 show_menu() {
     clear
     echo "======================================="
-    echo "||   CÀI ĐẶT ALL-IN-ONE BY CHANGG    ||"
+    echo "||    CÀI ĐẶT ALL-IN-ONE BY LUONG    ||"
 	echo "||  LƯU Ý: FREE/PREMIUM CỦA VIETBOT  ||"
     echo "======================================="
 	echo "||   CÀI ĐẶT AI - DLNA - UNISOUND    ||"
